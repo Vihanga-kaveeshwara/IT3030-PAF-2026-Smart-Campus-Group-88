307cb78885d444aad2c9af49a0695ab15c2d913d
Sasanka Bandara <sasankabandara33@gmail.com>
2026-04-21 07:21:14 +0530

    Update SecurityConfig to secure other endpoints and allow facilities API

--- smart-campus-api/src/main/java/com/smartcampus/smart_campus_api/config/SecurityConfig.java
+++ smart-campus-api/src/main/java/com/smartcampus/smart_campus_api/config/SecurityConfig.java
@@ -10,11 +10,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/health").permitAll()
-                .anyRequest().authenticated()
+                .requestMatchers("/api/facilities/**").permitAll() 
+                .anyRequest().authenticated() 
             );
         return http.build();
     }
