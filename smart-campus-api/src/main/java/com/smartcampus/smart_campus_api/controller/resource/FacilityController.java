575cb6577c20abcfc9c680031572c3b6bf5be60f
Sasanka Bandara <sasankabandara33@gmail.com>
2026-04-24 06:13:48 +0530

    fix: add CORS configuration to FacilityController

--- smart-campus-api/src/main/java/com/smartcampus/smart_campus_api/controller/resource/FacilityController.java
+++ smart-campus-api/src/main/java/com/smartcampus/smart_campus_api/controller/resource/FacilityController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/facilities") // Me API ekata yana pradhana URL eka
+@CrossOrigin(origins = "http://localhost:5173") 
+@RequestMapping("/api/facilities")
 public class FacilityController {
 
     @Autowired
